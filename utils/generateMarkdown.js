2046d96ef1f9e306dabc7b8ab7b770b213cdb501
DerVerruckte <michael.heinhold@gmail.com>
2021-09-16 16:14:42 -0600

    added dynamic sections

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,10 +10,60 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
+//sections that depend on user selection
+const generateInstall = installation => {
+  if(!installation){
+    return ''
+  } else {
+    return `
+  ## Installation
+  To install this on your computer type the following code into your terminal:
+  \`\`\`
+  ${installation}
+  \`\`\``
+  }
+}
+
+const generateUsage = usage => {
+  if(!usage){
+    return ''
+  } else {
+    return `
+  ## Usage
+  ${usage}`
+  }
+}
+
+const generateContribution = contribution => {
+  if(!contribution){
+    return ''
+  } else {
+    return `
+  ## How To Contribute
+  ${contribution}`
+  }
+}
+
+const generateTesting = testing => {
+  if(!testing){
+    return ''
+  } else {
+    return `
+  ## Testing
+  ${testing}`
+  }
+}
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+  # ${data.title}
+  ## Description
+  ${data.description}
+  ${generateInstall(data.installation)}
+  ${generateUsage(data.usage)}
+  ${generateContribution(data.contribution)}
+  ${generateTesting(data.testing)}
 `;
 }
 
